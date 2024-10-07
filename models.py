aeccaffa226e4bd55acdf2aecd8f9ed7871d74bb
YongFu Lin <felixlin0907@gmail.com>
2024-10-07 16:51:38 +1300

    index page

--- models.py
+++ models.py
@@ -11,18 +11,72 @@ class User(db.Model):
     password = db.Column(db.String(120), nullable=False)
     registered_on = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
-class Ticket(db.Model):
-    __tablename__ = 'tickets'
+class Concert(db.Model):
+    __tablename__ = 'concerts'
     id = db.Column(db.Integer, primary_key=True)
-    departure = db.Column(db.String(80), nullable=False)
-    destination = db.Column(db.String(80), nullable=False)
-    departure_time = db.Column(db.DateTime, nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    concert_name = db.Column(db.String(255), nullable=False)
+    artist = db.Column(db.String(255), nullable=False)
+    venue = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    concert_date = db.Column(db.DateTime, nullable=False)
+    ticket_price = db.Column(db.Float, nullable=False)
 
-class Order(db.Model):
-    __tablename__ = 'orders'
+class ConcertTicket(db.Model):
+    __tablename__ = 'concert_tickets'
     id = db.Column(db.Integer, primary_key=True)
+    concert_id = db.Column(db.Integer, db.ForeignKey('concerts.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    ticket_id = db.Column(db.Integer, db.ForeignKey('tickets.id'), nullable=False)
-    status = db.Column(db.String(20), nullable=False)
-    created_on = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    seat_number = db.Column(db.String(10))
+    ticket_price = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(20), nullable=False)  # '预订', '支付', '取消'
+    purchase_date = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    concert = db.relationship('Concert', backref='concert_tickets')
+    user = db.relationship('User', backref='concert_tickets')
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    movie_title = db.Column(db.String(255), nullable=False)
+    cinema_name = db.Column(db.String(255), nullable=False)
+    hall = db.Column(db.String(100), nullable=False)  # 影厅名称
+    show_time = db.Column(db.DateTime, nullable=False)
+    ticket_price = db.Column(db.Float, nullable=False)
+
+class MovieTicket(db.Model):
+    __tablename__ = 'movie_tickets'
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    seat_number = db.Column(db.String(10))
+    ticket_price = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(20), nullable=False)  # '预订', '支付', '取消'
+    purchase_date = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    movie = db.relationship('Movie', backref='movie_tickets')
+    user = db.relationship('User', backref='movie_tickets')
+
+class Flight(db.Model):
+    __tablename__ = 'flights'
+    id = db.Column(db.Integer, primary_key=True)
+    flight_number = db.Column(db.String(50), nullable=False)
+    departure_city = db.Column(db.String(100), nullable=False)
+    destination_city = db.Column(db.String(100), nullable=False)
+    departure_time = db.Column(db.DateTime, nullable=False)
+    arrival_time = db.Column(db.DateTime, nullable=False)
+    airline = db.Column(db.String(100), nullable=False)
+    ticket_price = db.Column(db.Float, nullable=False)
+
+class FlightTicket(db.Model):
+    __tablename__ = 'flight_tickets'
+    id = db.Column(db.Integer, primary_key=True)
+    flight_id = db.Column(db.Integer, db.ForeignKey('flights.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    seat_number = db.Column(db.String(10))
+    ticket_price = db.Column(db.Float, nullable=False)
+    cabin_class = db.Column(db.String(50))  # '经济舱', '商务舱', '头等舱'
+    status = db.Column(db.String(20), nullable=False)  # '预订', '支付', '取消'
+    purchase_date = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    flight = db.relationship('Flight', backref='flight_tickets')
+    user = db.relationship('User', backref='flight_tickets')
---
+++ static/js/global.js
@@ -0,0 +1,4 @@
+// 全局JS，用于导航栏等全局交互
+function showPlaceholder() {
+    alert("This feature is coming soon!");
+  }
